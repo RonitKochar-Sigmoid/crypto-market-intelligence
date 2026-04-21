24a8aafb7b98c17aecd908ab1286447a3b075a35
Ronit Kochar <ronit.kochar@sigmoidanalytics.com>
2026-04-21 14:06:32 +0530

    added timeout

--- airflow_dags/crypto_pipeline_dag.py
+++ airflow_dags/crypto_pipeline_dag.py
@@ -10,6 +10,7 @@ default_args = {
     'depends_on_past': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
+    'execution_timeout': timedelta(minutes=10)
 }
 
 # Define the secrets to be injected into the Pods
